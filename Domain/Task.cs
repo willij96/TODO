c9920774699eb6df92aa5b9aec897f4e14a50817
William <william.jj@hotmail.com>
2019-09-24 15:30:28 +0100

    Initial commit

---
+++ Domain/Task.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TODO.Domain
+{
+    class Task
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+
+        public DateTime? dueDate { get; set; }
+
+        public DateTime? Completed { get; set; }
+
+        public bool isCompleted
+        {
+            get
+            {
+                return Completed != null;
+            }
+        }
+      
+
+        public Task(int id, string title, DateTime dueDate)
+        {
+            Id = id;
+            Title = title;
+            this.dueDate = dueDate;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using static System.Console;
+using TODO.Domain;
+
+namespace TODO
+{
+    class Program
+    {
+        static Task[] taskList = new Task[100];        
+
+        static void Main(string[] args)
+        {
+                      
+            bool shouldRun = true;
+
+            int taskIdCounter = 1;
+            
+            while (true)
+            {
+                Clear();
+
+                WriteLine("1. Add todo");
+                WriteLine("2. List todos");
+                WriteLine("3. Exit");
+
+                ConsoleKeyInfo keyPressed = ReadKey(true);
+
+                Clear();
+
+                switch (keyPressed.Key)
+                {
+                    case ConsoleKey.D1:
+
+                        Write("Title: ");
+                        string title = ReadLine();
+
+                        Write("Due date (yyyy-mm-dd hh:mm): ");
+
+                        DateTime dueDate = DateTime.Parse(ReadLine());
+
+
+
+                        // TODO.Domain.Task task = new Task(title, dueDate);
+
+                        taskList[GetIndexPosition()] = new Task(taskIdCounter++, title, dueDate);
+
+
+
+                        break;
+
+                    case ConsoleKey.D2:
+
+                        WriteLine("ID      Title                      Due date                    Completed");
+                        WriteLine("------------------------------------------------------------------------");
+
+                        foreach (var task in taskList)
+                        {
+                            if (task == null) continue;
+
+                            WriteLine($"{task.Id} {task.Title}{task.dueDate.ToString().PadLeft(25, ' ')}");
+                        }
+
+                        WriteLine();
+
+                        ReadKey(true);
+
+                        break;
+
+                    case ConsoleKey.D3:
+
+                        shouldRun = false;
+
+                        break;
+                }
+            }
+        }
+
+        static int GetIndexPosition()
+        {
+            int result = -1;
+            for (int i = 0; i < taskList.Length; i++)
+            {
+                if (taskList[i] != null)
+                {
+                    continue;
+                }
+                if (taskList[i] == null)
+                {
+                    result = i;
+                    break;
+                }
+                if (result == -1)
+                {
+                    throw new Exception("No avalible position");
+                }
+            }
+            return result;
+        }
+    }
+}
